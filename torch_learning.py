1edce392763ef88183855d0a014112628772ffed
WatchingZ <168295553+WatchingZ@users.noreply.github.com>
2024-04-28 11:06:27 -0400

    Update and rename torch_deep_learning.py to torch_learning.py
    Removed train & test

---
+++ torch_learning.py
@@ -0,0 +1,160 @@
+import torch
+import torchmetrics
+import matplotlib.pyplot as plt
+
+import random
+
+def generate_samples(dataset, n_samples: int) -> list:
+    """
+    Generates a list of samples from the given dataset randomly
+    
+    Args: 
+    dataset (Any): dataset where samples will be extracted (must be unpackable)
+    n_samples (int): amount of samples to be extracted from dataset
+  
+    Returns:
+    A list consisting of random samples extracted from the dataset with a length of n_samples along with their corresponding label (can be unpacked)
+    """
+    
+    samples = []
+  
+    for sample, label in random.sample(list(dataset), k=n_samples):
+      samples.append((sample, label))
+  
+    return samples
+
+def plot_images(dataset, rows: int, columns: int, figsize: tuple, cmap: str, title=True, fontsize=10) -> None:
+    """
+    Plots a certain amount of images from the given dataset
+  
+    Args:
+    dataset (Any): dataset in which images are stored (must be unpackable, if length does not equal rows * columns, the first rows * columns images of the dataset will be plotted)
+    rows (int): how many rows of images the figure will consist of 
+    columns (int): how many columns of images the figure will consist of
+    figsize (tuple): what the resulting figure size of the figure will be
+    cmap (str): what the color maps of the plotted images will be
+    title (bool, optional): whether or not a title will be displayed for each image (defaults to True)
+    fontsize (int, optional): font size of the title (defaults to 10)
+  
+    Returns:
+    A matplotlib plot consisting of rows * columns images
+    """
+    
+    plt.figure(figsize=figsize)
+  
+    for i in range(rows * columns):
+      image, label = dataset[i]
+      
+      plt.subplot(rows, columns, i+1)
+  
+      if title:
+          plt.title(dataset.classes[label], fontsize=fontsize)
+      plt.imshow(image.squeeze(), cmap=cmap)
+    plt.show();
+
+def plot_image_predictions(predictions, dataset, rows: int, columns: int, figsize: tuple, fontsize=10, cmap=None) -> None:
+    """ Plots the image being predicted on and the models prediction on it 
+  
+    Args:
+    predictions (Any): the model's predictions on the dataset (must be in probability form)
+    dataset (Any): The dataset the model predicted on
+    rows (int): how many rows of images the figure will consist of 
+    columns (int): how many columns of images the figure will consist of 
+    figsize (tuple): the resulting figure size of the plot
+    fontsize (int, optional): the fontsize of the resulting plot (default is 10)
+    cmap (str, optional): the colormap of the resulting images
+  
+    Returns:
+    A matplotlib plot of all images and their respective predictions
+    """
+    
+    plt.figure(figsize=figsize)
+    
+    for i in range(rows * columns):
+      plt.subplot(rows, columns, i+1)
+  
+      image, label = dataset[i]
+      prediction = predictions[i]
+  
+      title = f"Predicted: {dataset.classes[prediction.argmax()]}: {prediction.max(): .2f} | Truth: {dataset.classes[label]}"
+      color = "green" if dataset.classes[prediction.argmax()] == dataset.classes[label] else "red"
+  
+      plt.title(title, fontsize=fontsize, c=color)
+      plt.imshow(image.squeeze(), cmap=cmap)
+  
+    plt.show();
+  
+
+def plot_loss(results, figsize=(15, 7)) -> None:
+    """
+    Plots a loss curve
+  
+    Args:
+        results (dict): dictionary containing list of values, e.g.
+            {"epoch": [...],
+             "loss": [...],}
+        figsize (tuple, optional): What the resulting figure size of the plot will be (defaults to (15, 7))
+        
+    Returns:
+    A matplotlib plot of the loss curves
+    """
+  
+    losses = results["loss"]
+    epochs = results["epoch"]
+  
+    plt.plot(epochs, losses, label="Loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+  
+    plt.show();
+
+def plot_linear_predictions(X: torch.Tensor, y: torch.Tensor, predictions=None, colors=['r','g'], figsize=(10, 7)) -> None:
+    """
+    Plots the linear predictions of a certain model
+  
+    Args:
+    X (torch.Tensor): the data inputed through the model
+    y (torch.Tensor): the labels of the data given
+    predictions (torch.Tensor): The predictions of the model
+    colors (list, optional): The colors of each of scatter plots (defaults to [r, g])
+    figsize (tuple, optional): The figure size of the returned plot
+  
+    Returns:
+    A Matplotlib scatter plot consisting of the data, labels and the models predictions
+    """
+    plt.figure(figsize=figsize)
+  
+    plt.scatter(X, y, c=colors[0], s=4, label="Data")
+    
+    if predictions is not None:
+        plt.scatter(X, predictions, c=colors[1], s=4, label="Predictions")
+  
+    plt.legend(prop={"size": 14})
+
+
+def make_predictions(model: torch.nn.Module, dataset, device: str):
+    """
+    Makes predictions using the given model on a given dataset
+  
+    Args: 
+    model (torch.nn.Module): the model predicting on the given dataset
+    dataset (Any): the dataset in which the model will be predicting on
+    device (str): specifies what device the data should be on
+  
+    Returns:
+    A list of predictions from the model (in raw logit form)
+    """
+    model.eval()
+  
+    with torch.inference_mode():
+      predictions = []
+      
+      for X, y in dataset:
+        X = X.to(device)
+        
+        prediction = model(X)
+  
+        predictions.append(prediction)
+  
+    return predictions
