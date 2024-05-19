39e2209532abc4de2223ed445d63483774d2fec2
WatchingZ <168295553+WatchingZ@users.noreply.github.com>
2024-05-18 21:52:10 -0400

    Update torch_learning.py
    fixed plot loss

--- torch_learning.py
+++ torch_learning.py
@@ -142,19 +142,13 @@ def plot_loss(results: dict, figsize=(15, 7)) -> None:
     A matplotlib plot of the loss curves
     """
     plt.figure(figsize=figsize)
-
-    plt.subplot(1, 2, 1)
+    
     plt.title("Train Loss")
-    plt.plot(results["epoch"], results["train_loss"], label="Loss")
+    plt.plot(results["epoch"], results["train_loss"], label="Train Loss")
+    plt.plot(results["epoch"], results["test_loss"], label="Test Loss")
     plt.xlabel("Epochs")
     plt.legend()
-
-    plt.subplot(1, 2, 2)
-    plt.title("Test Loss")
-    plt.plot(results["epoch"], results["test_loss"], label="Loss")
-    plt.xlabel("Epochs")
-    plt.legend()
-
+    
     plt.show();
 
 # --- Accuracy Evaluation Function --- #
